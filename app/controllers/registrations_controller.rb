0418e24ab7e8c1c9c912278130c0f9fe17c872d8
mnkhod <mnkhod.dev@gmail.com>
2025-08-23 18:43:10 +0800

    added registration resource for user sign up

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,23 @@
+class RegistrationsController < ApplicationController
+  allow_unauthenticated_access
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      redirect_to new_session_path, notice: "Successfully created user"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [:email_address, :password, :password_confirmation])
+  end
+end
